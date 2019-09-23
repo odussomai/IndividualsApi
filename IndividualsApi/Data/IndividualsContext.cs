b3e97087ebb3fa076db4e486136a513bce299880
Aleksandre <aleksandre.tsereteli@europebet.com>
2019-09-23 13:01:02 +0400

    added seed

--- IndividualsApi/Data/IndividualsContext.cs
+++ IndividualsApi/Data/IndividualsContext.cs
@@ -1,4 +1,5 @@
-﻿using IndividualsApi.Data.Entities;
+﻿using System;
+using IndividualsApi.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace IndividualsApi.Data
@@ -12,6 +13,56 @@ namespace IndividualsApi.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Individual>(i =>
+            {
+                i.HasData(new Individual
+                {
+                    Id = 1,
+                    DateOfBirth = new DateTime(1991, 5, 5),
+                    FirstName = "Aleksandre",
+                    LastName = "Tsereteli",
+                    PersonalId = "01024080411",
+                    Sex = BinarySex.Male,
+                    Image = "",
+                });
+
+                i.OwnsOne(c => c.City).HasData(new City
+                {
+                    Id = 1,
+                    Name = "Tbilisi",
+                });
+
+                //i.OwnsMany(p => p.PhoneNumbers).HasData(
+                //    new
+                //    {
+                //        Id = 1,
+                //        IndividualId = 1,
+                //        PhoneNumber = "598499901"
+                //    },
+                //    new 
+                //    {
+                //        Id = 2,
+                //        IndividualId = 1,
+                //        PhoneNumber = "577676767"
+                //    });
+
+
+            });
+
+            modelBuilder.Entity<Phone>().HasData(new
+            {
+                Id = 1,
+                IndividualId = 1,
+                PhoneNumber = "598499901",
+                Type = PhoneType.Mobile,
+            },
+            new
+            {
+                Id = 2,
+                IndividualId = 1,
+                PhoneNumber = "577676767",
+                Type = PhoneType.Home,
+            });
         }
 
         public DbSet<Individual> Individuals { get; set; }
---
+++ IndividualsApi/Migrations/20190923085920_seed.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IndividualsApi.Migrations
+{
+    public partial class seed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Individuals_Cities_CityId",
+                table: "Individuals");
+
+            migrationBuilder.DropTable(
+                name: "Cities");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Individuals_CityId",
+                table: "Individuals");
+
+            migrationBuilder.DropColumn(
+                name: "CityId",
+                table: "Individuals");
+
+            migrationBuilder.AddColumn<string>(
+                name: "City_Name",
+                table: "Individuals",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "Individuals",
+                columns: new[] { "Id", "DateOfBirth", "FirstName", "Image", "LastName", "PersonalId", "Sex", "City_Name" },
+                values: new object[] { 1, new DateTime(1991, 5, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), "Aleksandre", "", "Tsereteli", "01024080411", 0, "Tbilisi" });
+
+            migrationBuilder.InsertData(
+                table: "Phones",
+                columns: new[] { "Id", "IndividualId", "PhoneNumber", "Type" },
+                values: new object[] { 1, 1, "598499901", 1 });
+
+            migrationBuilder.InsertData(
+                table: "Phones",
+                columns: new[] { "Id", "IndividualId", "PhoneNumber", "Type" },
+                values: new object[] { 2, 1, "577676767", 2 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Phones",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Phones",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Individuals",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DropColumn(
+                name: "City_Name",
+                table: "Individuals");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CityId",
+                table: "Individuals",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Individuals_CityId",
+                table: "Individuals",
+                column: "CityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Individuals_Cities_CityId",
+                table: "Individuals",
+                column: "CityId",
+                principalTable: "Cities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
