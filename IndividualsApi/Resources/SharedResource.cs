d44c5541e89a067a54425d2943e1ac857b677e5e
Aleksandre <aleksandre.tsereteli@europebet.com>
2019-09-25 16:04:08 +0400

    Localization final touch

--- IndividualsApi/Resources/SharedResource.cs
+++ IndividualsApi/Resources/SharedResource.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace IndividualsApi.Resources
+namespace IndividualsApi
 {
     public class SharedResource
     {
--- IndividualsApi/Startup.cs
+++ IndividualsApi/Startup.cs
@@ -11,7 +11,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using IndividualsApi.Data.Services;
-using IndividualsApi.Resources;
 using Newtonsoft.Json;
 
 namespace IndividualsApi
